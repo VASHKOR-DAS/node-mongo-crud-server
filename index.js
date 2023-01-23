fb05b4b9d9c6ede51ab248f8a6a55854538f7e4e
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-01-23 16:05:48 +0600

    Mongodb And Send User Data To The Server1

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Mongodb And Send User Data To The Server
+const uri = "mongodb+srv://dbUser1:CIn42bwZCXBOMFBm@cluster0.wbb4jrx.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const userCollection = client.db('nodeMongoCrud').collection('users');
+        const user = {
+            name : 'Joya',
+            email : 'joya@gmail.com'
+        }
+        const result = await userCollection.insertOne(user);
+        console.log(result);
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.log(err));
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from node mongo crud server')
+});
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+})
